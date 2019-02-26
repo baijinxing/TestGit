e8aaa8ed562056fc3a2b1d4c2f46d6f26fdc93e7
baijinxing <1654352259@qq.com>
2019-02-26 15:32:33 +0800

    第二次更改

--- src/Hellworld.java
+++ src/Hellworld.java
@@ -1,3 +1,5 @@
 public class Hellworld {
-     
+     public static void main( String[] args){
+           System.out.println("ider真的是一个有趣的软件");
+     }
 }
---
+++ src/TestTwo.java
@@ -0,0 +1,6 @@
+public class TestTwo {
+    public static void main( String[] args){
+        System.out.println("ider真的是一个有趣的软件");
+        String yi = "ds";
+    }
+}
